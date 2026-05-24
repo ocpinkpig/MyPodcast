fd15940964bff914009b679064dc8d84d1148e4c
Admin <admin@song-wangs-mini.lan>
2026-05-24 14:42:06 -0700

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.mypodcast"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
